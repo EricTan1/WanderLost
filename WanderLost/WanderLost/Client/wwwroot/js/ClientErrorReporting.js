18ac601306262db9b5f6ff270e34de4124468c6e
Joshua Shaffer <xeio@live.com>
2022-04-30 18:16:01 -0400

    Report client side errors to server

---
+++ WanderLost/WanderLost/Client/wwwroot/js/ClientErrorReporting.js
@@ -0,0 +1,16 @@
+﻿let defaultLog = console.error;
+
+console.error = async function (e) {
+    defaultLog(e);
+    try {
+        let object = typeof(e.stack) === 'string' ? e.stack : e.toString();
+        await fetch('/ClientErrorReporting', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(object)
+        });
+    }
+    catch (e) {
+        defaultLog(e);
+    }
+}
---
+++ WanderLost/WanderLost/Server/Controllers/ClientErrorReportingController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WanderLost.Server.Controllers
+{
+    public class ClientErrorReportingController : Controller
+    {
+        private readonly ILogger<ClientErrorReportingController> _logger;
+
+        public ClientErrorReportingController(ILogger<ClientErrorReportingController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        [Route("/ClientErrorReporting")]
+        public IActionResult Index([FromBody] string? clientError)
+        {
+            if (!string.IsNullOrWhiteSpace(clientError))
+            {
+                _logger.LogError("Client Error: {clientError}", clientError);
+            }
+            return StatusCode(StatusCodes.Status200OK);
+        }
+    }
+}
